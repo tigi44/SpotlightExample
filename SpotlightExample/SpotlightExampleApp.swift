6edc1300505f800337295d51329d3e927904fa08
tigi44 <onlytg@naver.com>
2021-07-08 17:02:04 +0900

    init project

---
+++ SpotlightExample/SpotlightExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotlightExampleApp.swift
+//  SpotlightExample
+//
+//  Created by tigi KIM on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct SpotlightExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
